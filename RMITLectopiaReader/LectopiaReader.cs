fdc53a596499175c942e88e883b501dfd0dde9ea
PakkuDon <pakku.pd@gmail.com>
2015-03-16 20:01:59 +1100

    Added LectopiaReader class and some code to report progress updates
    - Created LectopiaReader class with some partially complete methods.
    - Added some code to the main method to test the LectopiaReader over an
      arbitrary range.

---
+++ RMITLectopiaReader/LectopiaReader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RMITLectopiaReader
+{
+    class LectopiaReader
+    {
+        // Constants
+        private const String BASE_URL = "https://lectopia.rmit.edu.au/lectopia/lectopia.lasso?ut=";
+
+        // Constructor
+        public LectopiaReader()
+        {
+            RecordingListURLs = new List<String>();
+            UnsuccessfulURLs = new List<String>();
+            URLsRead = 0;
+        }
+
+        // Properties / Instance vars
+        public List<String> RecordingListURLs { get; private set; }
+        public List<String> UnsuccessfulURLs { get; private set; }
+        public int URLsRead { get; private set; }
+
+        // -- Methods --
+        public void ReadCourseData(int start = 1, int end = Int16.MaxValue, IProgress<Double> callback = null)
+        {
+            URLsRead = 0;
+            Parallel.For(start, end,
+                new ParallelOptions { MaxDegreeOfParallelism = 10 },
+                i =>
+                {
+                    ReadRecordingList(i);
+                    URLsRead++;
+                    if (callback != null)
+                    {
+                        callback.Report((double)URLsRead / end * 100);
+                    }
+                });
+        }
+
+        public void ReadRecordingList(int id)
+        {
+            String URL = BASE_URL + id;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "HEAD";
+            request.AllowAutoRedirect = false;
+            request.Proxy = null;
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        RecordingListURLs.Add(URL);
+
+                        // TODO: Retrieve recording details
+                    }
+                }
+            }
+            catch (WebException)
+            {
+                // TODO: Log error
+                UnsuccessfulURLs.Add(URL);
+            }
+        }
+    }
+
+}
--- RMITLectopiaReader/Program.cs
+++ RMITLectopiaReader/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,29 @@ namespace RMITLectopiaReader
     {
         static void Main(string[] args)
         {
+            // Print heading
+            Console.WriteLine("RMIT Lectopia Reader");
+            Console.WriteLine("------------------------");
+            Console.WriteLine();
+
+            // Initialize necessary components
+            var reader = new LectopiaReader();
+            IProgress<Double> progressCallback = new Progress<Double>(progress =>
+            {
+                Console.Write("\r");
+                Console.Write("{0}% completed.", progress.ToString("#.##"));
+            });
+
+            // Perform lengthy reading operation
+            Console.WriteLine("Reading pages");
+            reader.ReadCourseData(end: 400, callback: progressCallback);
+
+            // Print statistics
+            var list = reader.RecordingListURLs;
+            Console.WriteLine();
+            Console.WriteLine("{0} successful reads.", reader.RecordingListURLs.Count());
+
+            Console.ReadLine();
         }
     }
 }
